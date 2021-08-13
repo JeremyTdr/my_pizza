bfdbc33f5c992fbbeea9fa0e2de70f37f9021825
Jeremy Tdr <jeremy.tdr@gmail.com>
2021-08-13 13:48:14 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'la jeremy': {
+        'ingredients': {'mozzarella': 3, 'pepper': 4, 'chorizo': 5},
+        'price': 12,
     }
 }
